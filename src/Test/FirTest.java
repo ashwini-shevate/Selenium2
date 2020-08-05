29a1c093e5787767cd77cc273ec1b0ddd48a38e2
admin <admin@admin-PC>
2020-08-05 15:50:11 +0530

    SeleniumDe

---
+++ src/Test/FirTest.java
@@ -0,0 +1,11 @@
+package Test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirTest {
+public static void main(String[] args) {
+	WebDriver driver=new ChromeDriver();
+	driver.get("https://www.facebook.com/");
+}
+}
